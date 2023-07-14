20cdf29fd6a79164edde7809c8a9844df53d78c0
bri <bcamesi@gmail.com>
2023-07-14 19:02:45 -0400

    Started content for sitebuildA

---
+++ frontend/src/Pages/WhiteLabeling/SiteBuildA.js
@@ -0,0 +1,13 @@
+//REQUEST A STAND-ALONE SITE
+
+function SiteBuildA(props) {
+    return (
+      <>
+        <a href="/">Back to Partner Page</a>
+        <h1>Request a Stand-Alone Site</h1>
+        <p>After you submit this form, someone from our team will reach out to you to start the process and clarify any additional details.</p>
+      </>
+    );
+  }
+  
+  export default SiteBuildA;
