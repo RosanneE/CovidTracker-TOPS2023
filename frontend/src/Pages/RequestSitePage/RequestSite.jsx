fdd0cf8a5efcc3c0dc8f813d61f4ed4582449c53
EdASalazar <edasalazar123@gmail.com>
2023-07-26 22:44:06 -0700

    first commit new branch

--- frontend/src/Pages/RequestSitePage/RequestSite.jsx
+++ frontend/src/Pages/RequestSitePage/RequestSite.jsx
@@ -1,6 +1,9 @@
-import SiteBuildB from "../../Components/RequestSite/SiteBuildB"
+import SiteBuildB from "../../Components/RequestSite/SiteBuildB";
+import { useState } from "react"
 
 export default function RequestSite(props) {
+    const [newSite, setNewSite] = useState()
+    
     return (
         <div>
             <h1>Request Site</h1>
