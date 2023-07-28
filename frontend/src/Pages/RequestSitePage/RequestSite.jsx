4244bb4775453475c282a0b31e7f1785763b7eca
EdASalazar <edasalazar123@gmail.com>
2023-07-28 16:02:17 -0700

    Setting up A B & C

--- frontend/src/Pages/RequestSitePage/RequestSite.jsx
+++ frontend/src/Pages/RequestSitePage/RequestSite.jsx
@@ -1,22 +1,43 @@
+import SiteBuildA from "../../Components/RequestSite/SiteBuildA/SiteBuildA";
 import SiteBuildB from "../../Components/RequestSite/SiteBuildB/SiteBuildB";
+import SiteBuildC from "../../Components/RequestSite/SiteBuildC/SiteBuildC"
 import { useState } from "react"
 
 export default function RequestSite(props) {
     const [newSite, setNewSite] = useState({
-    org_name: "",
-    contact_name: "",
-    email: "",
-    site_origin: "",
-    home_link: "",
-    positive_test_link: "",
-    negative_test_link: "",
-    more_info_link: "",
-
+        org_name: "",
+        contact_name: "",
+        email: "",
+        site_origin: "",
+        home_link: "",
+        positive_test_link: "",
+        negative_test_link: "",
+        more_info_link: "",
     });
+
+    const [pageNumber, setPageNumber] = useState(0)
     
     return (
         <div>
-           
-            <SiteBuildB/>
+            {pageNumber === 0 ? <SiteBuildA 
+                pageNumber={ pageNumber } 
+                setPageNumber={ setPageNumber } 
+                newSite={ newSite } 
+                setNewSite={ setNewSite }  
+                /> : "" }
+            {pageNumber === 1 ? <SiteBuildB 
+                pageNumber={ pageNumber } 
+                setPageNumber={ setPageNumber }  
+                newSite={ newSite } 
+                setNewSite={ setNewSite } 
+                /> : ""}
+            {pageNumber === 2 ? <SiteBuildC  
+                pageNumber={ pageNumber } 
+                setPageNumber={ setPageNumber } 
+                newSite={ newSite } 
+                setNewSite={ setNewSite } 
+
+            /> : ""}
+
         </div>
     )}
