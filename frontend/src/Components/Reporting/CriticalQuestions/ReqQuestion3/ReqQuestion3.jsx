e16a381533ff0b96c81e7ad22d4cb7ce33ba7280
bri <bcamesi@gmail.com>
2023-07-31 20:11:25 -0400

    Added calendar and buttons change color on select

--- frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion3/ReqQuestion3.jsx
+++ frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion3/ReqQuestion3.jsx
@@ -1,28 +1,70 @@
-import backArrow from "../../../../Images/Button - Back to Step 2.png"
-import ReqQuestionCalendar from "../ReqQuestion5/ReqQuestionCalendar"
-import "../CriticalQuestions.css"
+import React, { useState } from 'react';
+import backArrow from '../../../../Images/Button - Back to Step 2.png';
+import ReqQuestionCalendar from '../ReqQuestion5/ReqQuestionCalendar';
+import '../CriticalQuestions.css';
 
 export default function ReqQuestion3({ demoPage, setDemoPage }) {
- return ( 
+  const [selectedButton, setSelectedButton] = useState('');
+
+  const handleButtonClick = (selected) => {
+    setSelectedButton(selected);
+  };
+
+  return (
     <>
-    
-   
-    <div className="questionNumber">
-    <img src={backArrow} alt="Back Arrow" onClick={()=>setDemoPage(demoPage -1)}/>
-    Required Question 3 of 5
-    </div>
+      <div className="questionNumber">
+        <img
+          src={backArrow}
+          alt="Back Arrow"
+          onClick={() => setDemoPage(demoPage - 1)}
+        />
+        Required Question 3 of 5
+      </div>
 
-    <div className="questionAnswer">
-        <p><span className="question">When did you test?</span><span className="asterisk">*</span><br/>
-        <button className="selectButtonLeft">Today</button><button className="selectButtonMiddle">Yesterday</button><button className="selectButtonRight">Other</button></p>
-    </div>
+      <div className="questionAnswer">
+        <p>
+          <span className="question">When did you test?</span>
+          <span className="asterisk">*</span>
+          <br />
+          <button
+            className={`selectButtonLeft ${
+              selectedButton === 'Today' ? 'selected' : ''
+            }`}
+            onClick={() => handleButtonClick('Today')}
+          >
+            Today
+          </button>
+          <button
+            className={`selectButtonMiddle ${
+              selectedButton === 'Yesterday' ? 'selected' : ''
+            }`}
+            onClick={() => handleButtonClick('Yesterday')}
+          >
+            Yesterday
+          </button>
+          <button
+            className={`selectButtonRight ${
+              selectedButton === 'Other' ? 'selected' : ''
+            }`}
+            onClick={() => handleButtonClick('Other')}
+          >
+            Other
+          </button>
+        </p>
+      </div>
 
-    <div className="calendar">
-    <ReqQuestionCalendar />
-    </div>
+      <div className="calendar">
+        <ReqQuestionCalendar />
+      </div>
 
-    <div className="nextButtonDiv">
-        <button className="nextButton" onClick={()=> setDemoPage(demoPage + 1)}>Next</button>
-    </div>
+      <div className="nextButtonDiv">
+        <button
+          className="nextButton"
+          onClick={() => setDemoPage(demoPage + 1)}
+        >
+          Next
+        </button>
+      </div>
     </>
-)}
+  );
+}
--- frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion5/ReqQuestionCalendar.jsx
+++ frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion5/ReqQuestionCalendar.jsx
@@ -1,4 +1,29 @@
-export default function ReqQuestionCalendar(props) {
-    return ( 
-       <h3>Req Question Calendar</h3>
-   )}
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../CriticalQuestions.css';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+const ReqQuestionCalendar = () => {
+    const [selectedDate, setSelectedDate] = useState(null);
+  
+    const handleDateChange = (date) => {
+      setSelectedDate(date);
+    };
+  
+    return (
+      <div className="calendar">
+        <div className="calendarText">
+        Please select your test date:
+        </div>
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          dateFormat="dd/MM/yyyy"
+          inline // Use the inline prop to display the calendar directly
+        />
+      </div>
+    );
+  };
+  
+  export default ReqQuestionCalendar;
