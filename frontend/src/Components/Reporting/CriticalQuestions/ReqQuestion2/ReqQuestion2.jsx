3fc50cc1f16e05ad2e483076a8e12407e4d4f391
bri <bcamesi@gmail.com>
2023-08-06 20:06:51 -0400

    Added functions to questions and answers

--- frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion2/ReqQuestion2.jsx
+++ frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion2/ReqQuestion2.jsx
@@ -1,53 +1,67 @@
-import backArrow from "../../../../Images/Button - Back to Step 2.png"
-import iHealth from "../../../../Images/ihealth.png"
-import abbott from "../../../../Images/abbott.png"
-import flowFlex from "../../../../Images/flowflex.png"
-import quidel from "../../../../Images/quidel.png"
-import clinitest from "../../../../Images/clinitest.png"
-import roche from "../../../../Images/roche.png"
-import "../CriticalQuestions.css"
+import backArrow from "../../../../Images/Button - Back to Step 2.png";
+import stockImage from "../../../../Images/reportstartphoto.png";
+import tissue from "../../../../Images/sick-tissue.png";
+import happy from "../../../../Images/happy.png";
+import iHealth from "../../../../Images/ihealth.png";
+import abbott from "../../../../Images/abbott.png";
+import flowFlex from "../../../../Images/flowflex.png";
+import quidel from "../../../../Images/quidel.png";
+import clinitest from "../../../../Images/clinitest.png";
+import roche from "../../../../Images/roche.png";
+import "../CriticalQuestions.css";
+import { useState, useContext } from "react";
+import { FormContext } from "../../../../Context/FormContext";
 
 export default function ReqQuestion2({ demoPage, setDemoPage }) {
- return ( 
-    <>
-    <div className="questionNumber">
-    <img src={backArrow} alt="Back Arrow" onClick={()=> setDemoPage(demoPage -1)}/>
-    Required Question 2 of 5
-    </div>
+    const [selectedTest, setSelectedTest] = useState(null);
+    const { newUser, handleUserChange, handleUserSubmit } = useContext(FormContext);
+  
+    function handleTestSelection(testName) {
+      setSelectedTest(testName);
+  
+      // Also update the context
+      handleUserChange({
+        target: { name: "test_taken", value: testName },
+      });
+  
+      // Submit the form when the image is clicked
+      handleSubmit();
+    }
+  
+    function handleSubmit() {
+      // Check if a test is selected before moving to the next page
+      if (selectedTest !== null) {
+        console.log("Selected Test:", selectedTest); // Handle the selected test, e.g., send it to the server.
+  
+        // Move to the next page
+        setDemoPage(demoPage + 1);
+      } else {
+        // Display an error or alert to prompt the user to select a test before proceeding.
+      }
+    }
+  
+    return (
+      <>
+        <div className="questionNumber">
+          <img src={backArrow} alt="Back Arrow" onClick={() => setDemoPage(demoPage - 1)} />
+          Required Question 2 of 5
+        </div>
+  
+        <div className="questionAnswer">
+          <p>
+            <span className="question">Which test did you take?</span>
+            <span className="asterisk">*</span>
+            <br />
+            <h3 className="critQuestionsH3">iHealth</h3>
+              <img className="testImage" onClick={() => handleTestSelection("iHealth")} src={iHealth} alt="iHealth" style={{ maxWidth: "100%", width: "14rem", padding: "1rem" }} />
 
-    <div className="questionAnswer">
-        <p><span className="question">Which test did you take?</span><span className="asterisk">*</span><br/>
-        <table className="testTable">
-            <tr>
-                <td><h3 className="critQuestionsH3">iHealth</h3>
-                <p><img src={iHealth} alt="iHealth" style={{maxWidth:"100%", width:"14rem", padding:"1rem"}}/></p>
-                </td>
-                <td><h3 className="critQuestionsH3">Abbott BinaxNOW</h3>
-                <p><img src={abbott} alt="Abbott" style={{maxWidth:"100%", width:"14rem", padding:"1rem"}}/></p>
-                </td>
-            </tr>
-            <tr>
-            <td><h3 className="critQuestionsH3">Flowflex</h3>
-                <p><img src={flowFlex} alt="Flowflex" style={{maxWidth:"100%", width:"14rem", padding:"1rem"}}/></p>
-                </td>
-                <td><h3 className="critQuestionsH3">Quidel Quickvue</h3>
-                <p><img src={quidel} alt="Quidel" style={{maxWidth:"100%", width:"14rem", padding:"1rem"}}/></p>
-                </td>
-            </tr>
-            <tr>
-            <td><h3 className="critQuestionsH3">Siemens CLINITEST</h3>
-                <p><img src={clinitest} alt="Siemens Clinitest" style={{maxWidth:"100%", width:"14rem", padding:"1rem"}}/></p>
-                </td>
-                <td><h3 className="critQuestionsH3">Roche COVID-19 At-Home Test</h3>
-                <p><img src={roche} alt="Roche" style={{maxWidth:"100%", width:"14rem", padding:"0rem 1rem 1rem 1rem"}}/></p>
-                </td>
-            </tr>
-        </table>
-        <br/>
-        <button className="otherButton">Other</button>
-        {/* adding temp next page button  */}
-        <button onClick={()=>setDemoPage(demoPage + 1)}>Next Page</button>
-        </p>
-    </div>
-    </>
-)}
+            <h3 className="critQuestionsH3">Abbott BinaxNOW</h3>
+              <img className="testImage" onClick={() => handleTestSelection("Abbott BinaxNOW")} src={abbott} alt="Abbott" style={{ maxWidth: "100%", width: "14rem", padding: "1rem" }} />
+
+              <img className="testImage" onClick={() => handleTestSelection("Flowflex")} src={flowFlex} alt="Flowflex" style={{ maxWidth: "100%", width: "14rem", padding: "1rem" }} />
+            <button onClick={() => handleTestSelection("Other")} className="otherButton">Other</button>
+          </p>
+        </div>
+      </>
+    );
+  }
