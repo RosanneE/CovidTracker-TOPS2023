6815a5cede870f76d28d575ab2cb40469395901d
Brett Abramson <brett.m.abramson@gmail.com>
2023-08-07 18:58:08 -0700

    upon succesful form submission the form fields are reset - whitelabelsite

--- frontend/src/Context/FormContext.js
+++ frontend/src/Context/FormContext.js
@@ -5,7 +5,6 @@ export const FormContext = createContext();
 
 // Context provider component
 export const FormProvider = ({ children }) => {
-  
   const initialPartnerState = {
     org_name: "",
     contact_name: "",
@@ -20,23 +19,28 @@ export const FormProvider = ({ children }) => {
     custom_message: "",
     social_sharing_message: "",
     color_theme: "",
-  }
+  };
 
-  const storedPartnerState = JSON.parse(localStorage.getItem("partnerFormState"))
+  const storedPartnerState = JSON.parse(
+    localStorage.getItem("partnerFormState")
+  );
 
   //Partners
-  const [newSite, setNewSite] = useState(storedPartnerState || initialPartnerState);
+  const [newSite, setNewSite] = useState(
+    storedPartnerState || initialPartnerState
+  );
 
   useEffect(() => {
-    localStorage.setItem("partnerFormState", JSON.stringify(newSite))
-  }, [newSite])
+    localStorage.setItem("partnerFormState", JSON.stringify(newSite));
+  }, [newSite]);
+  
   const handlePartnerChange = (event) => {
     setNewSite({
       ...newSite,
       [event.target.name]: event.target.value,
     });
   };
-  // Either here or on review and submit page need to navigate to requestConfirmation page if POST success or specify error otherwise
+
   const handleReviewSubmit = (event) => {
     event.preventDefault();
 
@@ -52,7 +56,8 @@ export const FormProvider = ({ children }) => {
       .then((res) => res.json())
       .then((newPartner) => {
         console.log("New partner created: ", newPartner);
-        return true
+        setNewSite(initialPartnerState);
+        return true;
       })
       .catch((err) => {
         console.error("Error creating new partner: ", err);
