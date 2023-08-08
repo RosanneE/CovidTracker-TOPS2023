f65a48985c7be0db1d337520ed97438f20dcc92a
bri <bcamesi@gmail.com>
2023-08-08 11:45:07 -0400

    All questions are functional

--- frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion4/ReqQuestion4.jsx
+++ frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion4/ReqQuestion4.jsx
@@ -1,24 +1,70 @@
+import React, { useState, useContext } from "react";
 import backArrow from "../../../../Images/Button - Back to Step 2.png"
 import "../CriticalQuestions.css"
+import { FormContext } from "../../../../Context/FormContext";
 
-export default function ReqQuestion4({demoPage, setDemoPage}) {
- return ( 
-    <>
-    <div className="questionNumber">
-        <img src={backArrow} alt="Back Arrow" onClick={()=>setDemoPage(demoPage -1)}/>
-        Required Question 4 of 5
-    </div>
-
-
-
-    <div className="questionAnswer">
-        <p><span className="question">How old are you?</span><span className="asterisk">*</span><br/>
-        <input className="reqQuestionsInput" type="text" placeholder="e.g., 44"></input></p>
-    </div>
-
-    <div className="nextButtonDiv">
-        <button className="nextButton" onClick={()=> setDemoPage(demoPage + 1)}>Next</button>
-    </div>
-
-    </>
-)}
+export default function ReqQuestion4({ demoPage, setDemoPage }) {
+    const [age, setAge] = useState("");
+    const [isNextButtonClickable, setNextButtonClickable] = useState(false);
+    const { handleUserChange } = useContext(FormContext);
+  
+    const handleAgeChange = (event) => {
+      const inputAge = event.target.value;
+      setAge(inputAge);
+  
+      // Enable the Next button when the age input is not empty
+      if (inputAge !== "") {
+        setNextButtonClickable(true);
+      } else {
+        setNextButtonClickable(false);
+      }
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+  
+      // Update the context with the age value
+      handleUserChange({
+        target: { name: "age", value: age },
+      });
+  
+      // Move to the next page
+      setDemoPage(demoPage + 1);
+    };
+  
+    return (
+      <>
+        <div className="questionNumber">
+          <img src={backArrow} alt="Back Arrow" onClick={() => setDemoPage(demoPage - 1)} />
+          Required Question 4 of 5
+        </div>
+  
+        <div className="questionAnswer">
+          <p>
+            <span className="question">How old are you?</span>
+            <span className="asterisk">*</span>
+            <br />
+            <input
+              className="reqQuestionsInput"
+              type="text"
+              placeholder="e.g., 44"
+              value={age}
+              onChange={handleAgeChange}
+            />
+          </p>
+        </div>
+  
+        <div className="nextButtonDiv">
+          {/* Conditionally style the Next button based on clickability */}
+          <button
+            className={`nextButton ${isNextButtonClickable ? "clickable" : ""}`}
+            onClick={handleSubmit}
+            disabled={!isNextButtonClickable}
+            style={{ backgroundColor: isNextButtonClickable ? "#0058B7" : "" }} // Set background color to #0058B7 when clickable
+          >
+            Next
+          </button>
+        </div>
+      </>
+    );
+  }
--- frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion5/ReqQuestion5.jsx
+++ frontend/src/Components/Reporting/CriticalQuestions/ReqQuestion5/ReqQuestion5.jsx
@@ -1,23 +1,70 @@
 import backArrow from "../../../../Images/Button - Back to Step 2.png"
 import "../CriticalQuestions.css"
+import React, { useState, useContext } from "react";
+import { FormContext } from "../../../../Context/FormContext";
 
 export default function ReqQuestion5({ demoPage, setDemoPage }) {
- return ( 
-    <>
-    <div className="questionNumber">
-        <img src={backArrow} alt="Back Arrow" onClick={()=>setDemoPage(demoPage -1)}/>
-        Required Question 5 of 5
-    </div>
-
-
-
-    <div className="questionAnswer">
-        <p><span className="question">What is your home Zip Code?</span><span className="asterisk">*</span><br/>
-        <input className="reqQuestionsInput" type="text" placeholder="e.g., 11111"></input></p>
-    </div>
-
-    <div className="nextButtonDiv">
-        <button className="nextButton" onClick={()=> setDemoPage(demoPage + 1)}>Next</button>
-    </div>
-    </>
-)}
+    const [zipCode, setZipCode] = useState("");
+    const [isNextButtonClickable, setNextButtonClickable] = useState(false);
+    const { handleUserChange } = useContext(FormContext);
+  
+    const handleZipCodeChange = (event) => {
+      const inputZipCode = event.target.value;
+      setZipCode(inputZipCode);
+  
+      // Enable the Next button when the zip code input is not empty
+      if (inputZipCode !== "") {
+        setNextButtonClickable(true);
+      } else {
+        setNextButtonClickable(false);
+      }
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+  
+      // Update the context with the zip code value
+      handleUserChange({
+        target: { name: "zipCode", value: zipCode },
+      });
+  
+      // Move to the next page
+      setDemoPage(demoPage + 1);
+    };
+  
+    return (
+      <>
+        <div className="questionNumber">
+          <img src={backArrow} alt="Back Arrow" onClick={() => setDemoPage(demoPage - 1)} />
+          Required Question 5 of 5
+        </div>
+  
+        <div className="questionAnswer">
+          <p>
+            <span className="question">What is your home Zip Code?</span>
+            <span className="asterisk">*</span>
+            <br />
+            <input
+              className="reqQuestionsInput"
+              type="text"
+              placeholder="e.g., 11111"
+              value={zipCode}
+              onChange={handleZipCodeChange}
+            />
+          </p>
+        </div>
+  
+        <div className="nextButtonDiv">
+          {/* Conditionally style the Next button based on clickability */}
+          <button
+            className={`nextButton ${isNextButtonClickable ? "clickable" : ""}`}
+            onClick={handleSubmit}
+            disabled={!isNextButtonClickable}
+            style={{ backgroundColor: isNextButtonClickable ? "#0058B7" : "" }} // Set background color to #0058B7 when clickable
+          >
+            Next
+          </button>
+        </div>
+      </>
+    );
+  }
