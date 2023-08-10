121bc7a11a0c5a972ce408a45b42902c1c656152
Brett Abramson <brett.m.abramson@gmail.com>
2023-08-10 11:04:02 -0700

    sorted months coming back from API and determining cases per month now. Seems to be working as intended

--- frontend/src/Components/DataChart/DataChart.jsx
+++ frontend/src/Components/DataChart/DataChart.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 import { Line } from "react-chartjs-2";
-import Chart from "chart.js/auto"
+import Chart from "chart.js/auto";
 
 const DataChart = () => {
   const [govData, setGovData] = useState({ months: [], cases: [] });
   const [countyFipsCode, setCountyFipsCode] = useState("");
 
-  const fetchData = () => {
-    fetch(
-      `https://data.cdc.gov/resource/n8mc-b4w4.json?$limit=100000&county_fips_code=${countyFipsCode}`
-    )
-      .then((response) => {
-        // console.log(response)
-        return response.json();
-      })
-      .then((data) => {
-        // console.log(data)
-        // Process the fetched data and update state
-        const months = data.map((entry) => entry.case_month);
-        const cases = data.length;
-        // console.log(months);
-        // console.log(cases);
-        setGovData({ months: months, cases: cases });
-        // console.log(govData)
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://data.cdc.gov/resource/n8mc-b4w4.json?$limit=1000000&county_fips_code=${countyFipsCode}`
+      );
+      const data = await response.json();
+
+      const caseCounts = {};
+
+      data.forEach((entry) => {
+        const month = entry.case_month;
+        if (caseCounts[month]) {
+          caseCounts[month] += 1;
+        } else {
+          caseCounts[month] = 1;
+        }
       });
+
+      setGovData({ months: Object.keys(caseCounts), caseCounts });
+    } catch (error) {
+      console.error("Error fetching datt:", error);
+    }
   };
+
+  const sortedMonths = govData.months.sort((a, b) => new Date(a) - new Date(b))
 
   return (
     <div>
@@ -42,18 +45,18 @@ const DataChart = () => {
         {govData.months.length > 0 && (
           <Line
             data={{
-              labels: govData.months,
+              labels: sortedMonths,
               datasets: [
                 {
                   label: "# of Cases",
-                  data: govData.cases,
+                  data: sortedMonths.map(month => govData.caseCounts[month] || 0),
                   backgroundColor: "rgba(255, 99, 132, 0.2)",
                   borderColor: "rgba(255, 99, 132, 1)",
                   borderWidth: 1,
                 },
-              ]
+              ],
             }}
-            />
+          />
         )}
       </div>
     </div>
@@ -62,3 +65,20 @@ const DataChart = () => {
 
 export default DataChart;
 // 48453 is Travis County FIPS code
+
+// .then((response) => {
+//   // console.log(response)
+//   return response.json();
+// })
+// .then((data) => {
+//   console.log(data);
+//   // Process the fetched data and update  state
+//   const months = data.map((entry) => entry.case_month);
+//   const cases = data.length;
+//   console.log(months);
+//   console.log(cases);
+//   setGovData({ months: months, cases: cases });
+// })
+// .catch((error) => {
+//   console.error("Error fetching data:", error);
+// });
