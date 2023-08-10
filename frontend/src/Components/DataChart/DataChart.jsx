279d080adf18a205bafc2a25a1b53943e32027a9
Brett Abramson <brett.m.abramson@gmail.com>
2023-08-09 22:17:52 -0700

    Chart.js is now correctly displaying. Added import Chart fromt chart.js/auto and changed return

--- frontend/src/Components/DataChart/DataChart.jsx
+++ frontend/src/Components/DataChart/DataChart.jsx
@@ -1,52 +1,34 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Line } from "react-chartjs-2";
-
+import Chart from "chart.js/auto"
 
 const DataChart = () => {
   const [govData, setGovData] = useState({ months: [], cases: [] });
   const [countyFipsCode, setCountyFipsCode] = useState("");
 
   const fetchData = () => {
-    fetch(`https://data.cdc.gov/resource/n8mc-b4w4.json?$limit=100000&county_fips_code=${countyFipsCode}`)
-      .then(response => {
+    fetch(
+      `https://data.cdc.gov/resource/n8mc-b4w4.json?$limit=100000&county_fips_code=${countyFipsCode}`
+    )
+      .then((response) => {
         // console.log(response)
-        return response.json()
+        return response.json();
       })
-      .then(data => {
+      .then((data) => {
         // console.log(data)
         // Process the fetched data and update state
-        const months = data.map(entry => entry.case_month);
-        const cases = data.length
-        console.log(months)
-        console.log(cases)
+        const months = data.map((entry) => entry.case_month);
+        const cases = data.length;
+        // console.log(months);
+        // console.log(cases);
         setGovData({ months: months, cases: cases });
         // console.log(govData)
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   };
 
-  const renderChart = () => {
-    const chartData = {
-      labels: govData.months,
-      datasets: [
-        {
-          label: "# of Cases",
-          data: govData.cases,
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderColor: "rgba(255, 99, 132, 1)",
-          borderWidth: 1,
-        },
-      ],
-    };
-    return <Line data={chartData} />;
-  };
-
-  useEffect(() => {
-    renderChart();
-  }, [govData]);
-  
   return (
     <div>
       <input
@@ -56,9 +38,27 @@ const DataChart = () => {
         onChange={(event) => setCountyFipsCode(event.target.value)}
       />
       <button onClick={fetchData}>Get Data</button>
-      <canvas id="myChart" width="400" height="200"></canvas>
+      <div>
+        {govData.months.length > 0 && (
+          <Line
+            data={{
+              labels: govData.months,
+              datasets: [
+                {
+                  label: "# of Cases",
+                  data: govData.cases,
+                  backgroundColor: "rgba(255, 99, 132, 0.2)",
+                  borderColor: "rgba(255, 99, 132, 1)",
+                  borderWidth: 1,
+                },
+              ]
+            }}
+            />
+        )}
+      </div>
     </div>
   );
 };
 
 export default DataChart;
+// 48453 is Travis County FIPS code
