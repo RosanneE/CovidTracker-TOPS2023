2ee45aa43eccf1e4cad7f5f48bd93a128baaa56b
Brett Abramson <brett.m.abramson@gmail.com>
2023-08-12 14:08:03 -0700

    grid layed out and buttons in place. Need dynamic content now.

--- frontend/src/Components/WhiteLabelSite/ExploreDemo/ExploreDemo.jsx
+++ frontend/src/Components/WhiteLabelSite/ExploreDemo/ExploreDemo.jsx
@@ -2,31 +2,35 @@ import { useState } from "react";
 import "./ExploreDemo.css";
 
 const ExploreDemo = (props) => {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
 
-  const [activeSlideIndex, setActiveSlideIndex] = useState(0)
-
-  const handleCarouselChange = index => {
-    setActiveSlideIndex(index)
-  }
+  const handleCarouselChange = (index) => {
+    setActiveSlideIndex(index);
+  };
 
   return (
     <div className="explore-modal-overlay">
-      <div className="explore-modal">
-        <span onClick={props.handleModalClose}>
-          &times;
-        </span>
-        <div className="">
+      <div className="explore-modal-container">
+        <span onClick={props.handleModalClose}>&times;</span>
+        <div className="explore-modal">
           <div className="explore-header">
             <h3>Explore the Tool</h3>
             <p>Table of Contents (needs down arrow)</p>
           </div>
           <div className="explore-body">
-            <div className="explore-leftSide"></div>
+            <div className="explore-leftSide">ScreenShot Here</div>
             <div className="explore-rightSide">
               Text here should be variable based, moves with the carosel
             </div>
           </div>
-          <div className="explore-footer">Footer</div>
+          <div className="explore-footer">
+            <div className="explore-button-container">
+              <button className="blueButton" id="explore-previousButton">
+                Previous
+              </button>
+              <button className="blueButton">Next</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
